ec4859114a774c1a26a3d48133f57aba41801463
Konashuk <konan4ukv@gmail.com>
2023-12-14 17:07:53 +0200

    add createSelectors

--- src/components/contacklist/contactList.js
+++ src/components/contacklist/contactList.js
@@ -1,29 +1,20 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { List, Button, Item } from './contactList.styled';
-import { selectContacts, selectStatusFilter } from 'redux/selectors';
+import { selectFilterContacts } from 'redux/selectors';
 import { deleteContacts } from 'redux/operation';
 
 export const ContactList = () => {
-  const dataContacts = useSelector(selectContacts);
-  const dataFilters = useSelector(selectStatusFilter);
+  const visibleContacts = useSelector(selectFilterContacts);
   const dispatch = useDispatch();
 
-  const filterContacts = () => {
-    return dataContacts.filter(({ name }) => {
-      return name.toLowerCase().includes(dataFilters.toLowerCase());
-    });
-  };
-
   const updateCont = id => {
-    console.log(dataContacts);
-    console.log(filterContacts());
     dispatch(deleteContacts(id));
   };
 
   return (
     <List>
-      {filterContacts() &&
-        filterContacts().map(({ id, name, phone }) => {
+      {visibleContacts &&
+        visibleContacts.map(({ id, name, phone }) => {
           return (
             <Item key={id}>
               {`${name}: ${phone}`}
